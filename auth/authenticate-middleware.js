e6ee65de8cff4d954db6ccd661edb92e198b5396
Tomer du Sautoy <tomerdusautoy@gmail.com>
2019-08-24 15:00:20 -0600

    finished mvp

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,26 @@
+require('dotenv').config();
+
+const jwt = require('jsonwebtoken');
+
 /* 
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, `${process.env.JSONSECRET}`, (error, decodedToken) => {
+      if (error) {
+        res.status(401).json({ error: 'permission denied!' })
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    })
+  } else {
+    res.status(400).json({ error: 'No token provided' })
+  }
 };
